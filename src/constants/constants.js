b5286ad13ae44477beced05f00821e3798f3170c
ahmedezzaouia <35857662+ahmedezzaouia@users.noreply.github.com>
2022-01-09 12:34:10 +0100

    add description

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 export const projects = [
   {
     title: "Shopify Store for a client ",
-    description: "Shopify Website for a client",
+    description: "Shopify Store fo a client with a custom design and logo ",
     image: "/images/soukakech.png",
-    tags: ["Shopify", "Liquid", "JavaScript"],
+    tags: ["Shopify", "Liquid", "JavaScript", "Photoshop"],
     source:
       "https://upwork-usw2-prod-agora-file-storage.s3.us-west-2.amazonaws.com/profile/portfolio/thumbnail/c4a5d65fbd114954ac4cd75e03380897?response-content-disposition=inline%3B%20filename%3D%22image_fixed_width%22%3B%20filename%2A%3Dutf-8%27%27image_fixed_width&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBQaCXVzLXdlc3QtMiJIMEYCIQCWKHcRznZeqhlqmvSRE09T35mk1QZTMhzxUPK8KEd%2FOAIhAIKMUeU6xWu30Ov3Lx7NJT1%2Bf%2BehHURODUWMsmR3M%2FKPKs0ECB0QABoMNzM5OTM5MTczODE5Igzxwzos23st8GIf%2Bh4qqgTxzTpu7hUPsasEbYClHil4r3iRog51kJ%2BWTityU90d%2B6laT0ObzCNi8lke4JcEC4wP0q7YZ7efIPOabdzYMe6NXQd317ghBfGbqaoJjCofXrnsx3aWTxCvyaSyXtM4N6M0%2BSOkkzCCXwPjQT%2BuuW4LIrOolB1JpthlqV30gPaNlyEawqbg0WZd8IzhVO0QHxSmlfCc%2FE6QMk4rvMi%2BbhK73jucNrYGrGKKzdBck6Y17H9fG3b%2BUu%2FbXk3l31M4TkX6hyNhsNbp%2BzM26CTFQUhSuvwDJ6SsZFs%2FYd3ztRmCiFYItGdnnKeUqXHMgoC2hfvm%2BvGPSVSjNRsq7Ot2fj4n81mGMf3vyiRkdqchN1LOCsWyheZYe4swUII6uLwUuRsmdWz7Yed5IvlVtn5Zsxg2vlg%2BhTiBAwCLWkei89iPoiTCL%2FQ1IntHacBbgage2gUgCOtJlzOUWwRZjSWOH122qe5uUYTbLI8HSET9iG3idQL6hSApa4YX1IJQLSCkPR517HwsDb2tyUjszT2cKu4V4bT%2FyP2qxt6ak2KptJBdjuwqNnJqCe7vWRM33jp6VQHXz5mBBXM5MwoIAdd1B0V6gbuecUHDZE3D1pKTkqe2C%2FdFNSriiyE%2FXk12bFNBZ7zve%2Ftu%2FgtIMFqDcIGUubSud%2FXblRGfqfXzp6YblgpK2DqHBfQkekf6Dgq%2BV7h6olZGQiRCuF6Rw473NzLmTbFfFPMAqvVcDyAN3TCXub2OBjqmAQNgSK7ACSjWaFyCC6PsEoToiFuKSJiKcmQWjRkn4itEr5uxpVMH6fV8%2FukpsXEs8S6h8NszLcIbGNLctBuvecNF8hgMzNd7vZJt9J5eRzHe6yjLZY2HiTzFKhwot5id5f8Ew74otzSjZE2ls%2FDAGGidkwgX5OcYYc7L1mgcNjBD7LLw0RjOERGEnUzNP0hoTmPGuyFRQqC1OC44W%2FyWsIeMKS0L2aE%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20211231T230226Z&X-Amz-SignedHeaders=host&X-Amz-Expires=900&X-Amz-Credential=ASIA2YR6PYW5Y2ZSL3XR%2F20211231%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Signature=49518026e691455074ed209ecee59a16ff459ac27c4baa827a352d0710abf4c7",
     visit:
@@ -21,7 +21,7 @@ export const projects = [
   },
   {
     title: "Landing Page",
-    description: "custom landing page design ",
+    description: "Responsive Landing Page design with HTML and CSS ",
     image: "/images/landing-page.jpeg",
     tags: ["Advance Css", "Html"],
     visit: "https://github.com/ahmedezzaouia/Landing-page-",
